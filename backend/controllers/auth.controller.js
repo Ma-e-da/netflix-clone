aacb6d6cd4169543cdbda6789f4c53209a441080
Yoshie Maeda <cassisism@gmail.com>
2024-10-15 21:57:30 +0900

    Signupでpostをする

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,53 @@
+import { User } from "../models/user.model.js";
+
 export async function signup(req, res) {
-  res.send("Signup");
+  try {
+    const { email, passoword, username } = req.body;
+
+    if (!email || !password || !username) {
+      return res.status(400).json({
+        success: false,
+        message: "すべてのフィールドを入力してください",
+      });
+    }
+    const emailRefex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res
+        .status(400)
+        .json({ success: false, message: "無効なメールです" });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: false,
+        message: "パスワードは６文字以下にしてください",
+      });
+    }
+    const existingUserByEmail = await User.findOne({ email: email });
+
+    if (existingUserByEmail) {
+      return res.status(400).json({
+        success: false,
+        message: "メールアドレスはすでに存在します",
+      });
+    }
+    const PROFILE_PICS = ["/avatar1.png", "/avatar2.png", "/avatar3.png"];
+    const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)];
+    const newUser = new User({
+      email,
+      password,
+      username,
+      image,
+    });
+
+    await newUser.save();
+  } catch (error) {
+    console.log("Signupコントローラーでエラー", error.message);
+    res
+      .status(500)
+      .json({ success: false, message: "インターナルサーバーエラー" });
+  }
 }
 
 export async function login(req, res) {
